c9ebffef67b5f2b117e15c8d798ec89c390973c6
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-12 16:55:18 +0100

    complete the logic for the train ticket

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,7 @@ const ticketCarriageEl = document.getElementById('ticketCarriage')
 const ticketCodeEl = document.getElementById('ticketCode')
 const ticketPriceEl = document.getElementById('ticketPrice')
 
+
 formEl.addEventListener("submit", function (e) {
   e.preventDefault();
 
@@ -17,34 +18,36 @@ formEl.addEventListener("submit", function (e) {
   const name = nameEl.value;
   const km = kmEl.value;
   const age = ageEl.value;
-  console.log(name, km, age);
 
   const ticketNameValue = nameEl.value;
-  // const ticketDiscountValue = ticketDiscountEl.value;
-  // const ticketCarriageValue = ticketCarriageEl.value;
-  // const ticketCodeValue = ticketCodeEl.value;
-  // const ticketPriceValue = ticketPriceEl.value;
-
+ 
   // calcolo del prezzo senza sconto
   const priceKm = 0.21;
   const basePrice = km * priceKm;
 
+  // variabile per il calcolo della carrozza
+  const carriage = Math.floor(Math.random() * 11);
+  console.log(carriage);
+  
   if (age === "1") {
     const minorDiscount = basePrice * 0.2;
     const minorDiscountTotal = basePrice - minorDiscount;
-    ticketDiscountEl.innerHTML = 'Sconto del 20%'
     ticketNameEl.innerHTML = ticketNameValue
+    ticketDiscountEl.innerHTML = 'Sconto del 20%'
+    ticketCarriageEl.innerHTML = carriage
     ticketPriceEl.innerHTML = minorDiscountTotal.toFixed(2)
 
   } else if (age === "2") {
     const overDiscount = basePrice * 0.4;
     const overDiscountTotal = basePrice - overDiscount;
-    ticketDiscountEl.innerHTML = 'Sconto del 40%'
     ticketNameEl.innerHTML = ticketNameValue
+    ticketDiscountEl.innerHTML = 'Sconto del 40%'
+    ticketCarriageEl.innerHTML = carriage
     ticketPriceEl.innerHTML = overDiscountTotal.toFixed(2)
   } else {
     ticketNameEl.innerHTML = ticketNameValue
     ticketDiscountEl.innerHTML = 'Prezzo standard'
+    ticketCarriageEl.innerHTML = carriage
     ticketPriceEl.innerHTML = basePrice.toFixed(2)
   }
 });
