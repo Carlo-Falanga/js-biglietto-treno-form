056b7dd3d7b8564ff3ade5c96c5149098854cb5c
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-12 14:45:15 +0100

    add variables and aventlistener structure

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,24 @@
 console.log('It works');
+
+
+
+
+
+const formEl = document.querySelector('form')
+const nameEl = document.getElementById('name')
+const kmEl = document.getElementById('km')
+const ageEl = document.getElementById('age')
+
+const priceKm = 0.21;
+const basePrice = kmEl * priceKm;
+
+
+formEl.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const name = nameEl.value;
+    const km = kmEl.value;
+    const age = ageEl.value;
+    console.log(name, km, age);
+    
+})
